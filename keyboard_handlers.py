06bc4894eb2a46a5aae06248cd838c9489bf2647
gerasimovtltsu <theavg@yandex.ru>
2024-03-26 19:11:36 +0400

    feat: complete main functionality. Now consultant can answer to users and users can send messages to bot

--- keyboard_handlers.py
+++ keyboard_handlers.py
@@ -1,38 +1,35 @@
-from telegram import Update
-from telegram.ext import ContextTypes
+from aiogram import types, Dispatcher
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 SPECIALIST_CHAT_ID = 288957466
 active_consultations = {}
 
-from telegram import Update
-from telegram.ext import ContextTypes
+class UserState(StatesGroup):
+    in_consultation = State()
 
-async def handle_consultation(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    context.user_data['in_consultation'] = True
-    active_consultations[SPECIALIST_CHAT_ID] = update.effective_chat.id
-    await update.message.reply_text('Вы вошли в режим консультации. Напишите ваш вопрос, и специалист вам ответит.')
+async def handle_consultation(message: types.Message, state: FSMContext):
+    await state.set_state(UserState.in_consultation)
+    active_consultations[SPECIALIST_CHAT_ID] = message.chat.id
+    await message.answer('Вы вошли в режим консультации. Напишите ваш вопрос, и специалист вам ответит.')
 
-async def forward_to_specialist(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if context.user_data.get('in_consultation'):
-        await context.bot.forward_message(chat_id=SPECIALIST_CHAT_ID, from_chat_id=update.effective_chat.id, message_id=update.message.message_id)
-        await update.message.reply_text("Ваш запрос был отправлен консультанту. Пожалуйста, ожидайте ответа.")
+async def forward_to_specialist(message: types.Message, state: FSMContext):
+    if await state.get_state() == UserState.in_consultation.state:
+        await message.bot.forward_message(chat_id=SPECIALIST_CHAT_ID, from_chat_id=message.chat.id, message_id=message.message_id)
+        await message.answer("Ваш запрос был отправлен консультанту. Пожалуйста, ожидайте ответа.")
 
-async def forward_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.reply_to_message:
-        await context.bot.forward_message(chat_id=update.message.reply_to_message.forward_from.id, from_chat_id=update.effective_chat.id, message_id=update.message.message_id)
-        await update.message.reply_text("Ответ от консультанта отправлен вам.")
+async def forward_to_user(message: types.Message, state: FSMContext):
+    if message.reply_to_message:
+        await message.bot.forward_message(chat_id=message.reply_to_message.forward_from.id, from_chat_id=message.chat.id, message_id=message.message_id)
+        await message.answer("Ответ от консультанта отправлен вам.")
 
-async def answer_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if len(context.args) >= 2:
-        try:
-            user_id = int(context.args[0])
-            message_text = " ".join(context.args[1:])
-            await second_bot.send_message(chat_id=user_id, text=message_text)
-            await update.message.reply_text("Ответ отправлен пользователю")
-        except ValueError:
-            await update.message.reply_text("Некорректный ID пользователя")
-    else:
-        await update.message.reply_text("Использование: /answer {id_пользователя} {ваш ответ}")
+async def end_consultation(message: types.Message, state: FSMContext):
+    if await state.get_state() == UserState.in_consultation.state:
+        await state.clear()
+        await message.answer('Консультация завершена.')
 
-async def handle_bot_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Здесь будет информация о боте.')
+def register_handlers(dp: Dispatcher):
+    dp.register_message_handler(handle_consultation, commands=['start_consultation'], state='*')
+    dp.register_message_handler(forward_to_specialist, state=UserState.in_consultation)
+    dp.register_message_handler(forward_to_user, state=UserState.in_consultation)
+    dp.register_message_handler(end_consultation, commands=['end_consultation'], state=UserState.in_consultation)
--- main.py
+++ main.py
@@ -1,81 +1,81 @@
-import logging
+import asyncio
 import json
+import logging
 
-from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
-
-from config import BOT_TOKEN
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.fsm.context import FSMContext
+# from aiogram.utils import executor
+from config import BOT_TOKEN, ADMIN_BOT_TOKEN
 import keyboard_handlers
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+logging.basicConfig(level=logging.INFO)
 
-# Чтение конфигурации для клавиатуры
+admin_to_user_map = {} # словарь для хранения соответствия между сообщениями в чате админа и идентификаторами чатов пользователей
+
 with open("keyboard.json", "r", encoding='utf8') as kbd_layout:
     kbd_json = json.load(kbd_layout)
+
 keyboard = ReplyKeyboardMarkup(**kbd_json)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Добро пожаловать! Выберите действие на появивщейся клавиатуре. Для повторного вызова меню отправьте команду /start или /menu",
+bot = Bot(token=BOT_TOKEN)
+admin_bot = Bot(token=ADMIN_BOT_TOKEN)
+dp = Dispatcher()
+
+admin_dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.answer(
+        "Добро пожаловать! Выберите действие на появивщейся клавиатуре. Для повторного вызова меню отправьте команду /start или /menu",
         reply_markup=keyboard
     )
 
-async def forward_all_messages_to_specialist(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if context.user_data.get('in_consultation') and update.effective_chat.id != keyboard_handlers.SPECIALIST_CHAT_ID:
-        await context.bot.forward_message(chat_id=keyboard_handlers.SPECIALIST_CHAT_ID, from_chat_id=update.effective_chat.id, message_id=update.message.message_id)
-        await update.message.reply_text(f"Ваше сообщение было отправлено специалисту. ID пользователя: {update.effective_chat.id}. Для того, чтобы специалист мог связаться с Вами - откройте возможность писать людям не в Ваших контактах")
-    else:
-        await update.message.reply_text(f"Вы не вошли в режим консультации или Вы являетесь администратором", reply_markup=keyboard)
+@dp.message(lambda message: message.text == 'Консультация со специалистом')
+async def handle_consultation(message: types.Message, state: FSMContext):
+    await keyboard_handlers.handle_consultation(message, state)
 
-async def end_consultation(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if context.user_data.get('in_consultation'):
-        context.user_data['in_consultation'] = False
-        await update.message.reply_text(f'Консультация завершена. ID пользователя: {update.effective_chat.id}')
+@dp.message(lambda message: message.text == 'Информация о боте')
+async def handle_bot_info(message: types.Message, state: FSMContext):
+    await keyboard_handlers.handle_bot_info(message, state)
 
-async def list_open_requests(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Здесь вы можете получить список всех открытых заявок из какого-то хранилища данных
-    # Например, можно использовать базу данных или какой-то другой метод хранения данных
-    open_requests = []
+@dp.message(lambda message: message.from_user.id == keyboard_handlers.SPECIALIST_CHAT_ID)
+async def forward_to_user(message: types.Message, state: FSMContext):
+    await keyboard_handlers.forward_to_user(message, state)
 
-    if open_requests:
-        message = "Список открытых заявок:\n"
-        for request in open_requests:
-            message += f"- Заявка от {request['user_id']}: {request['text']}\n"
-    else:
-        message = "Нет открытых заявок."
+@dp.message()
+async def forward_all_messages_to_specialist(message: types.Message, state: FSMContext):
+    await keyboard_handlers.forward_to_specialist(message, state)
 
-    await update.message.reply_text(message)
+@dp.message(Command('end_consultation'))
+async def end_consultation(message: types.Message, state: FSMContext):
+    await keyboard_handlers.end_consultation(message, state)
 
-async def answer_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if len(context.args) >= 2:
-        try:
-            request_id = int(context.args[0])  # ID заявки
-            message_text = " ".join(context.args[1:])  # Текст ответа
-            await context.bot.send_message(chat_id=request_id, text=message_text)
-            await update.message.reply_text("Ответ на заявку отправлен пользователю")
-        except ValueError:
-            await update.message.reply_text("Некорректный ID заявки")
-    else:
-        await update.message.reply_text("Использование: /answer {ID_заявки} {ваш ответ}")
+@dp.message(Command('open_requests'))
+async def list_open_requests(message: types.Message, state: FSMContext):
+    await keyboard_handlers.list_open_requests(message, state)
+
+@dp.message(Command('answer'))
+async def answer_request(message: types.Message, state: FSMContext):
+    await keyboard_handlers.answer_request(message, state)
+
+async def forward_to_admin(message: types.Message):
+    forwarded_message = await admin_bot.forward_message(chat_id=ADMIN_CHAT_ID, from_chat_id=message.chat.id, message_id=message.message_id)
+    admin_to_user_map[forwarded_message.message_id] = message.chat.id
+
+@admin_dp.message(lambda message: message.reply_to_message and message.reply_to_message.message_id in admin_to_user_map)
+async def handle_admin_response(message: types.Message):
+    user_chat_id = admin_to_user_map[message.reply_to_message.message_id]
+    await admin_bot.send_message(chat_id=user_chat_id, text=message.text)
+
+@dp.message()
+async def handle_message(message: types.Message):
+    await forward_to_admin(message)
+
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-    
-    start_handler = CommandHandler(['start', 'menu'], start)
-    application.add_handler(start_handler)
-
-    application.add_handler(MessageHandler(filters.Regex('^Консультация со специалистом$'), keyboard_handlers.handle_consultation))
-    application.add_handler(MessageHandler(filters.Regex('^Информация о боте$'), keyboard_handlers.handle_bot_info))
-
-    application.add_handler(MessageHandler(filters.Text() & filters.ChatType.PRIVATE, forward_all_messages_to_specialist))
-    application.add_handler(MessageHandler(filters.User(user_id=keyboard_handlers.SPECIALIST_CHAT_ID) & filters.ChatType.PRIVATE, keyboard_handlers.forward_to_user))
-    
-    application.add_handler(CommandHandler('answer', answer_request))
-    application.add_handler(CommandHandler('end_consultation', end_consultation))
-    application.add_handler(CommandHandler('open_requests', list_open_requests))
-    
-    application.run_polling()
+    asyncio.run(main())
